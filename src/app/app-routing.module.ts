613d2767250d317c00524727d84eeec236cc8b57
HandreiFontana <HandreiFontana>
2023-08-22 15:06:35 -0300

    chore: home, signIn and default layout directories created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { DefaultComponent } from './_layouts/default/default.component'
+import { HomeComponent } from './pages/authentication/home/home.component'
+import { SignInComponent } from './pages/authentication/sign-in/sign-in.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: DefaultComponent,
+    children: [
+      {
+        path: "home",
+        component: HomeComponent,
+      }
+    ]
+  },
+  {
+    path: "login",
+    component: SignInComponent,
+  },
+
+  { path: "**", redirectTo: "/sign-in" },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './pages/authentication/home/home.component';
+import { SignInComponent } from './pages/authentication/sign-in/sign-in.component';
+import { DefaultComponent } from './_layouts/default/default.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SignInComponent,
+    DefaultComponent
   ],
   imports: [
     BrowserModule,
